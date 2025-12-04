4d223e2c3c8aba4dfb4d3c464cf5f2bc04f1e7a1
anol123 <anol.roy123@gmail.com>
2025-12-05 00:05:23 +0530

    Added the cache functionalities

--- src/cache.rs
+++ src/cache.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, sync::{Arc, Mutex}, time::Instant};
+use std::{collections::HashMap, sync::{Arc, Mutex}, time::{Duration, Instant}};
 
 pub struct CacheEntry{
     value : Vec<u8>,
@@ -8,10 +8,25 @@ pub struct InMemoryCache{
     store: Arc<Mutex<HashMap<String, CacheEntry>>>,
 }
 impl InMemoryCache{
-    pub fn new(){
-        
+    pub fn new()->Self{
+        Self { store: Arc::new(Mutex::new(HashMap::new())) }
     }
-    pub fn insert(key:String){
-        println!("Insert called with key {}",key);
+    pub fn insert_into(&mut self, key:String, value: Vec<u8>, ttl: Duration){
+        //let expires_at = Instant::now() + ttl;
+        let entry = CacheEntry{
+            value, 
+            expiry_at: Instant::now() + ttl };
+        self.store.lock().unwrap().insert(key,entry);
+    }
+
+    pub fn get(&self, key:&str)-> Option<Vec<u8>>{
+        self.store.lock().unwrap().get(key).and_then(|entry|{
+            if Instant::now()< entry.expiry_at{
+                Some(entry.value.clone())
+            }
+            else{
+                None
+            }
+        })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use crate::cache::InMemoryCache;
 mod cache;
 fn main() {
     let cache = InMemoryCache::new();
-    InMemoryCache::insert("Anol".to_string());
+    
 }
