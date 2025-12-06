2d70804fed39bcec8cc89777cc1007e3345b6bc6
anol123 <anol.roy123@gmail.com>
2025-12-06 19:33:46 +0530

    Eexected get method

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,6 @@ mod cache;
 fn main() {
     let mut cache = InMemoryCache::new();
     cache.insert("Anol".to_string(),b"CSE".to_vec(), Duration::from_secs(5));
+
+    println!("{:?}",cache.get("Anol"));
 }
