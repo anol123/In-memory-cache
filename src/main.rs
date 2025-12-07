64879602027cb3907f011ce7598cf831cb9c7a7b
anol123 <anol.roy123@gmail.com>
2025-12-07 18:22:30 +0530

    wip

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     cache.insert("abc".to_string(),b"NOC".to_vec(), Duration::from_secs(6));
     cache.insert("xyz".to_string(),b"we".to_vec(), Duration::from_secs(5));
     cache.insert("pqr".to_string(),b"vbvb".to_vec(), Duration::from_secs(15));
+    cache.insert("Anol".to_string(),b"CSE".to_vec(), Duration::from_secs(5));
 
     println!("{:?}",cache.get("Anol"));
 }
