31d017953c24a2e343fc61f1c62576379438c42a
anol123 <anol.roy123@gmail.com>
2025-12-08 22:05:52 +0530

    working

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,11 @@ mod cache;
 fn main() {
     let mut cache = InMemoryCache::new();
     cache.insert("Anol".to_string(),b"CSE".to_vec(), Duration::from_secs(5));
-    cache.insert("abc".to_string(),b"NOC".to_vec(), Duration::from_secs(6));
-    cache.insert("xyz".to_string(),b"we".to_vec(), Duration::from_secs(5));
+    // cache.insert("abc".to_string(),b"NOC".to_vec(), Duration::from_secs(6));
+    // cache.insert("xyz".to_string(),b"we".to_vec(), Duration::from_secs(5));
     cache.insert("pqr".to_string(),b"vbvb".to_vec(), Duration::from_secs(15));
-    cache.insert("Anol".to_string(),b"CSE".to_vec(), Duration::from_secs(5));
+    //cache.insert("Anol".to_string(),b"CSE".to_vec(), Duration::from_secs(5));
 
     println!("{:?}",cache.get("Anol"));
+    println!("{:?}",cache.get("pqr"));
 }
